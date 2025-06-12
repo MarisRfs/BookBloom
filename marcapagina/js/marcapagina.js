96c93e41e344ce9ea5eee2f0e08ee932bab71c5c
amolina00 <amoreschideoliveira@gmail.com>
2025-06-12 07:38:31 -0300

    marcapagina.js

--- marcapagina/js/marcapagina.js
+++ marcapagina/js/marcapagina.js
@@ -1,7 +1,20 @@
 document.getElementById('marcaPaginaBtn').addEventListener('click', function () {
-  document.getElementById('marcaPagina').classList.add('ativo');
-});
+    document.getElementById('marcaPagina').classList.add('ativo');
+  });
+  
+  document.querySelector('.fechar-btn').addEventListener('click', function () {
+    document.getElementById('marcaPagina').classList.remove('ativo');
+  });
 
-document.querySelector('.fechar-btn').addEventListener('click', function () {
-  document.getElementById('marcaPagina').classList.remove('ativo');
-});
+// categorias
+function toggleCategorias() {
+    const container = document.getElementById('categorias-container');
+    container.style.display = container.style.display === 'none' ? 'flex' : 'none';
+  }
+  
+  // Inicia oculto:
+  document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('categorias-container').style.display = 'none';
+  });
+
+  
